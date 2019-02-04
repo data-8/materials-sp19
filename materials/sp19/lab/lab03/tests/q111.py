c521f14a0565fcab57e3acc8ce4aa0e7a5ffe82b
Niraj Rao <niraj.a.rao@berkeley.edu>
2019-02-03 21:15:24 -0800

    sp19/lab/lab03.push

---
+++ materials/sp19/lab/lab03/tests/q111.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Hint 1: Try replacing the substring 'hi' with 'ma'
+          >>> new_word
+          'matchmaker'
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
