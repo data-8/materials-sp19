11fb8d43626ed951eaecfc8b07c8275576bc24af
Niraj Rao <niraj.a.rao@berkeley.edu>
2019-02-07 23:31:10 -0800

    sp19/hw/hw03.push

---
+++ materials/sp19/hw/hw03/tests/q3_3.py
@@ -0,0 +1,30 @@
+test = {
+  'name': 'Question 3_3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> type(movers)
+          int
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> 0 < movers <= 52
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
