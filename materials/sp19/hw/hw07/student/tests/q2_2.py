b2e9dc58f9d4af1170773aa7c7ea2611168007dd
Niraj Rao <niraj.a.rao@berkeley.edu>
2019-03-08 15:24:01 -0800

    sp19/hw/hw07.push

---
+++ materials/sp19/hw/hw07/student/tests/q2_2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q2_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> type(observed_test_stat) == float
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
