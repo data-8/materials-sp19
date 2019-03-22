c5807d25b31fe6f6c83e0e18b8d45902d4a0487d
Niraj Rao <niraj.a.rao@berkeley.edu>
2019-03-22 14:39:46 -0700

    sp19/hw/hw08.push

---
+++ materials/sp19/hw/hw08/tests/q2_5.py
@@ -0,0 +1,39 @@
+test = {
+  'name': 'Question 2_5',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Make sure answer your answer is between 1 and 3!
+          >>> 1 <= candidates_tied <= 3
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    },
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> candidates_tied == 1
+          True
+          """,
+          'hidden': True,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ materials/sp19/hw/hw08/tests/q2_6.py
@@ -0,0 +1,39 @@
+test = {
+  'name': 'Question 2_6',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Make sure answer your answer is between 1 and 3!
+          >>> 1 <= cutoff_one_percent <= 3
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    },
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> cutoff_one_percent == 3
+          True
+          """,
+          'hidden': True,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
