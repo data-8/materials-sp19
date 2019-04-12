f63bba32486a28a9de99f39b15ad77a0314df0c9
Niraj Rao <niraj.a.rao@berkeley.edu>
2019-04-12 10:51:13 -0700

    sp19/hw/hw10.push

---
+++ materials/sp19/hw/hw10/tests/q1_6.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q1_6',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> len(regression_changes) == 3
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
